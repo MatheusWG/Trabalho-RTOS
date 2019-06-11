155faa5d38f5a65d6efa1b57a6f80446a7571a87
Matheus <matheusweltermail@gmail.com>
2019-06-10 23:50:24 -0300

    hospital

---
+++ hospital.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <time.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+
+void delay(int number_of_seconds) { 
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * number_of_seconds; 
+  
+    // Stroing start time 
+    clock_t start_time = clock(); 
+  
+    // looping till required time is not acheived 
+    while (clock() < start_time + milli_seconds) 
+        ; 
+}
+
+void medir_sinais_vitais(void *pvParameters) {
+    while(1) {
+        int batimentos = rand() % (150 + 1 - 0) + 0;
+        printf("Batimentos: %d\n", batimentos);
+
+        int pressao1 = rand() % (16 + 1 - 0) + 0;
+        int pressao2 = rand() % (14 + 1 - 0) + 0;
+        printf("Pressao: %d", pressao1);
+        printf(" por %d\n", pressao2);
+        delay(1);
+        vTaskDelay(10 / portTICK_RATE_MS);
+    }
+}
+
+void injetar_medicamento(void *pvParameters) {
+    while(1) {
+        printf("Injetando medicamento\n");
+        vTaskDelay(5000 / portTICK_RATE_MS);
+    }
+}
+ 
+void app_main() {
+    xTaskCreate(&medir_sinais_vitais, "medir_sinais_vitais", 2048, NULL, 1, NULL);
+    xTaskCreate(&injetar_medicamento, "injetar_medicamento", 2048, NULL, 2,NULL);
+}
